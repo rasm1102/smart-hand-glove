41f4401edab6c93d96381ebf6c1663de6cf74d81
rasm1102 <115459557+rasm1102@users.noreply.github.com>
2022-10-10 14:41:04 +0530

    Add files via upload

---
+++ adminpage.php
@@ -0,0 +1,54 @@
+ <!DOCTYPE html>
+<html>
+<style>
+body {
+  background-image: url('https://media.istockphoto.com/photos/abstract-blur-soft-focus-blue-color-interior-of-modern-cleaning-picture-id1313300720?k=20&m=1313300720&s=170667a&w=0&h=C39LBz27u4BQXprOivKSm5-QTGJxki1euJdu9IoykpQ=');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+
+
+</style>
+<hr>
+<center><h1>SMART HAND GLOVE</h1></center>
+<hr>
+<?php
+
+
+echo "<h2>INRODUCTION :</h2><h3><br>
+Due to the COVID-19 pandemic, patients were unable to walk outside for their routine medical examinations for more than a year.
+Older people, women who are pregnant, and kids who have health issues should routinely monitor their health.
+Rural residents lack adequate transportation options for routine testing.
+Healthcare professionals who come into direct contact with patients for health checks, such as doctors and nurses, were impacted by COVID-19.
+</h3><br>";
+echo "<h2> TECHNOLOGY USED:</h2><br><h3> Obtaining values such as BPM, Temperature, Oxygen Level, and Flex Value by interfacing a number of sensors with the microcontroller.
+
+Sensors Used: Max30100(Pulse Oximeter), Flex sensor, RTC module.
+
+ The measured values are shown on an OLED display.
+
+ To ensure that patients don't forget to take their medications on time, the RTC module is utilized as a medication remainder.
+
+  An IoT platform called Thingspeak is utilised to facilitate communication between medical professionals and patients.</h3><br>
+";
+echo "<h2>SOLUTION:</h2><br><h3>
+We have designed a gadget that will use wearable technology to incorporate the temperature, oxygen level, and heartbeat in order to solve this issue.
+
+The details of patients can be pushed into cloud using IoT which makes it easier for the doctors to check the patient’s temperature, oxygen level and heartbeat without physical contact.
+
+Compared to other smart watch technologies on the market, this one might be more affordable.
+
+This gadget can be easily accessible by the patients</h3>
+";
+?> 
+
+
+</body>
+</html>
+<hr>
+<center><h1> !!! CHECK PRODUCT RESULT !!!  </h1></center>
+"<center> <h1><a href="https://thingspeak.com/channels/1838739/private_show">Click here</a></h1></center>";
+ 
+ 
+ 
